4df39f3c31ce87cbc5cc2a29efe2be36a3b1794a
Vitor Diogo <vitorpdiogo@gmail.com>
2024-11-29 18:16:22 -0300

    feat: add structure and configs

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+
+db = SQLAlchemy()
+ma = Marshmallow()
+migrate = Migrate()
+
+
+def create_app(instance_path: str = 'instance') -> Flask:
+    instance_path = os.path.abspath(instance_path)
+    app = Flask(__name__, instance_path=instance_path)
+
+    config = Config(instance_path)
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+
+    # blueprints register
+    from .api.task_api import task_api
+
+    app.register_blueprint(task_api, url_prefix='/tasks')
+
+    return app
---
+++ app/models/task.py
@@ -0,0 +1,18 @@
+from app import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    id_deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        server_onupdate=db.func.now(),
+    )
+
+    def __repr__(self) -> str:
+        return f'<Task {self.title}>'
