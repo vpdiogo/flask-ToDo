dd7c99b6860bc3e3511eea254dacef87f27aa77c
Vitor Diogo <vitorpdiogo@gmail.com>
2024-12-01 10:42:37 -0300

    feat: add tests

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import json
 
 
 class Config:
-    def __init__(self, path_instance: str) -> None:
+    def __init__(self, path_instance: str, testing: bool) -> None:
         settings_file = os.path.join(path_instance, 'settings.json')
 
         if os.path.exists(settings_file):
@@ -11,7 +11,7 @@ class Config:
                 config_data: dict = json.load(f)
             self.DEBUG: bool = config_data.get('DEBUG', False)
             self.SQLALCHEMY_DATABASE_URI: str = config_data.get(
-                'SQLALCHEMY_DATABASE_URI'
+                'SQLALCHEMY_DATABASE_URI_TEST' if testing else 'SQLALCHEMY_DATABASE_URI'
             )
             self.SECRET_KEY: str = config_data.get('SECRET_KEY')
             self.SQLALCHEMY_TRACK_MODIFICATIONS: bool = config_data.get(
